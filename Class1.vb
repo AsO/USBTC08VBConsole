5b2be0e68ae29462dac43aac3ffcc02addc0f597
AsO <aso.asri@gmail.com>
2013-02-28 22:57:20 +0900

    First runnable

---
+++ Class1.vb
@@ -0,0 +1,204 @@
+﻿Imports System
+Imports System.Threading
+Imports System.Runtime.InteropServices
+Imports USBTC08VBConsole.picotech.USBTC08API
+
+Public Class Win32Interop
+    <DllImport("crtdll.dll")>
+    Public Shared Function _kbhit() As Integer
+        '
+    End Function
+End Class
+
+Namespace picotech.USBTC08
+    Public Class USBTC08Console
+        Private ReadOnly _handle As Short
+
+        Public info As USBTC08API.UnitInfo
+        Shared tempReadings(8) As Single
+
+        Private Sub openUSBTC08(ByVal sender As System.Object, ByVal e As System.EventArgs)
+            Dim handle As Short = USBTC08API.OpenUnit()
+            Console.Write(handle)
+            'info.size = Marshal.SizeOf(info)
+            Dim infook As Short = USBTC08API.GetUnitInfo(handle, info)
+            Console.Write(infook)
+        End Sub
+
+        Private Sub getUSBTC08()
+            Dim unitResult As Long
+            Dim handle As Integer = 1
+            Dim ofFlag As Integer
+            Dim tempUnits As Integer = 1
+
+            For n = 0 To 8 - 1
+                unitResult = USBTC08API.SetChannel(handle, n, "K")
+                Debug.Print(unitResult)
+            Next
+
+            unitResult = USBTC08API.GetSingle(handle, tempReadings, ofFlag, tempUnits)
+            Debug.Print(unitResult & " " & ofFlag)
+
+            For n = 0 To 8 - 1
+                Debug.Print(tempReadings(n))
+            Next
+
+        End Sub
+
+        '/****************************************************************************
+        ' * Read the device information
+        ' ****************************************************************************/
+        Private Sub GetDeviceInfo()
+            Dim line As System.Text.StringBuilder = New System.Text.StringBuilder(USBTC08API.MAX_INFO_CHARS)
+
+            If _handle >= 0 Then
+                USBTC08API.GetFormattedInfo(_handle, line, USBTC08API.MAX_INFO_CHARS)
+                Console.WriteLine("{0}", line)
+            End If
+        End Sub
+
+        '/****************************************************************************
+        ' * Read temperature information from the unit
+        ' ****************************************************************************/
+        Private Sub GetValues()
+            Dim status As Short
+            Dim tempbuffer() As Single = New Single(8) {}
+            Dim overflow As Short
+            Dim lines As Integer = 0
+
+            Console.WriteLine()
+            '// label the columns
+            For chan As Short = 1 To USBTC08API.MAX_CHANNELS
+                Console.Write("Chan{0}:    ", chan)
+            Next
+
+            Console.WriteLine()
+
+            Do
+                status = USBTC08API.GetSingle(_handle, tempbuffer, overflow, USBTC08API.TempUnit.CENTIGRADE)
+
+                If status = PICO_OK Then
+                    For chan As Short = 1 To USBTC08API.MAX_CHANNELS
+                        Console.Write("{0:0.0000}   ", tempbuffer(chan))
+                    Next
+                    Console.WriteLine()
+                    Thread.Sleep(1000)
+                Else
+                    Dim err As Short = USBTC08API.GetLastError(_handle)
+                    Console.Write("GetValues:ERR CODE:{0}", err)
+                End If
+
+                lines += 1
+                If lines > 9 Then
+                    Console.WriteLine("Hit any key to stop....")
+                    Console.WriteLine("Cold Junction Temperature: {0:0.0000}   ", tempbuffer(0))
+
+                    lines = 0
+                    For chan As Short = 1 To USBTC08API.MAX_CHANNELS
+                        Console.Write("Chan{0}:    ", chan)
+                    Next
+                    Console.WriteLine()
+                End If
+            Loop While Win32Interop._kbhit() = 0
+
+            Dim ch As Char = (Console.ReadKey().KeyChar)       ' // use up keypress
+            status = USBTC08API.StopUnit(_handle)
+        End Sub
+
+        '/****************************************************************************
+        ' *  Set channels 
+        ' ****************************************************************************/
+        Private Sub SetChannels()
+            Dim status As Short
+
+            For channel As Short = 0 To USBTC08API.MAX_CHANNELS
+                status = USBTC08API.SetChannel(_handle, channel, USBTC08API.TC_TYPE_K)
+                If status <> PICO_OK Then
+                    Dim err As Short = USBTC08API.GetLastError(_handle)
+                    Console.Write("SetChannels:ERR CODE:{0}", err)
+                End If
+            Next
+        End Sub
+
+        '/****************************************************************************
+        '*  Run
+        '****************************************************************************/
+        Public Sub Run()
+            ' //// main loop - read key and call routine
+            Dim ch As Char = " "
+            While ch <> "X"
+                Console.WriteLine()
+                Console.WriteLine("I - Device Info")
+                Console.WriteLine("G - Get Temperatures")
+                Console.WriteLine("X - exit")
+                Console.WriteLine("Operation:")
+
+                ch = Char.ToUpper(Console.ReadKey(True).KeyChar)
+
+                Console.WriteLine()
+
+                Select Case ch
+                    Case "I"
+                        GetDeviceInfo()
+
+                    Case "G"
+                        SetChannels()
+                        GetValues()
+
+                    Case "X"
+                        '/* Handled by outer loop */
+
+                    Case Else
+                        Console.WriteLine("Invalid operation")
+                End Select
+            End While
+        End Sub
+
+        Public Sub New(handle As Short)
+            _handle = handle
+        End Sub
+
+    End Class
+
+    Module Module1
+        '/****************************************************************************
+        '*  WaitForKey
+        '*  Wait for user's keypress
+        '****************************************************************************/
+        Public Sub WaitForKey()
+            While (Not Console.KeyAvailable)
+                Thread.Sleep(100)
+            End While
+
+            If Console.KeyAvailable Then
+                Console.ReadKey(True) '// clear the key 
+            End If
+        End Sub
+
+        Public Sub Main()
+            Console.WriteLine("TC08 driver example program")
+            Console.WriteLine("Version 1.0")
+            Console.WriteLine()
+            '//open unit and show splash screen
+            Console.WriteLine("Opening the device...")
+
+            Dim handle As Short = USBTC08API.OpenUnit()
+            Console.WriteLine("Handle: {0}", handle)
+            If handle = 0 Then
+                Console.WriteLine("Unable to open device")
+                Console.WriteLine("Error code : {0}", handle)
+                WaitForKey()
+            Else
+                Console.WriteLine("Device opened successfully")
+                Console.WriteLine()
+
+                Dim consoleExample As USBTC08Console = New USBTC08Console(handle)
+                consoleExample.Run()
+
+                USBTC08API.CloseUnit(handle)
+            End If
+
+        End Sub
+
+    End Module
+End Namespace
---
+++ USBTC08Imports.vb
@@ -0,0 +1,130 @@
+﻿'/**************************************************************************
+'*
+'* Filename:    TC08Imports.cs
+'*
+'* Copyright:   Pico Technology Limited 2011
+'*
+'* Author:      CPY
+'*
+'* Description:
+'*   This file contains all the .NET wrapper calls needed to support
+'*   the console example. It also has the enums and structs required
+'*   by the (wrapped) function calls.
+'*
+'* History:
+'*    23/05/2011 	CPY	Created
+'*
+'* Revision Info: "file %n date %f revision %v"
+'*						""
+'*
+'***************************************************************************/
+
+Imports System
+Imports System.Runtime.InteropServices
+Imports System.Text
+Imports System.IO
+
+Namespace picotech
+    Public Class USBTC08API
+
+#Region "Constants"
+        Private Const _DRIVER_FILENAME As String = "usbtc08.dll"
+        Public Const MAX_INFO_CHARS As Short = 256
+        Public ReadOnly TC_Type As String() = {"X", "B", "E", "J", "K", "N", "R", "S", "T"}
+        Public Const MAX_CHANNELS As Integer = 8
+        Public Const TC_TYPE_K As Char = "K"
+        Public Const PICO_OK As Integer = 1
+#End Region
+
+#Region "Driver enums"
+        Public Enum TempUnit As Short
+            CENTIGRADE
+            FAHRENHEIT
+            KELVIN
+            RANKINE
+        End Enum
+
+#End Region
+
+#Region "Structs"
+        Public Structure UnitInfo
+            Public size As Short
+            Public DriverVersion As Short
+            Public PicoppVersion As Short
+            Public HardwareVersion As Short
+            Public picoVar As Short
+            Public szSerial As String
+            Public szCalDate As String
+        End Structure
+#End Region
+
+#Region "Driver Imports"
+#Region "Callback delegates"
+
+#End Region
+
+        <DllImport(_DRIVER_FILENAME, EntryPoint:="usb_tc08_open_unit")>
+        Public Shared Function OpenUnit() As Short
+            '
+        End Function
+
+        <DllImport(_DRIVER_FILENAME, EntryPoint:="usb_tc08_close_unit")>
+        Public Shared Function CloseUnit( _
+            ByVal handle As Short) As Short
+            '
+        End Function
+
+        <DllImport(_DRIVER_FILENAME, EntryPoint:="usb_tc08_run")>
+        Public Shared Function Run( _
+            ByVal handle As Short, _
+            interval As Integer) As Short
+            '
+        End Function
+
+        <DllImport(_DRIVER_FILENAME, EntryPoint:="usb_tc08_stop")>
+        Public Shared Function StopUnit( _
+            ByVal handle As Short) As Short
+            '
+        End Function
+
+        <DllImport(_DRIVER_FILENAME, EntryPoint:="usb_tc08_get_last_error")>
+        Public Shared Function GetLastError( _
+            ByVal handle As Short) As Short
+            '
+        End Function
+
+        <DllImport(_DRIVER_FILENAME, EntryPoint:="usb_tc08_get_unit_info")>
+        Public Shared Function GetUnitInfo( _
+            ByVal handle As Short, _
+            ByRef unit_info As UnitInfo) As Short
+            '
+        End Function
+
+        <DllImport(_DRIVER_FILENAME, EntryPoint:="usb_tc08_get_formatted_info")>
+        Public Shared Function GetFormattedInfo( _
+            ByVal handle As Short, _
+            unit_info As StringBuilder, _
+            string_length As Short) As Short
+            '
+        End Function
+
+        <DllImport(_DRIVER_FILENAME, EntryPoint:="usb_tc08_set_channel")>
+        Public Shared Function SetChannel( _
+            ByVal handle As Short, _
+            ByVal channel As Short, _
+            ByVal tc_type As Char) As Short
+            '
+        End Function
+
+        <DllImport(_DRIVER_FILENAME, EntryPoint:="usb_tc08_get_single")>
+        Public Shared Function GetSingle( _
+            ByVal handle As Short, _
+            ByVal temp As Single(), _
+            ByRef overflow_flags As Short, _
+            ByVal units As TempUnit) As Short
+        End Function
+
+#End Region
+
+    End Class
+End Namespace
